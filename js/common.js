675c6fcf9df91985c6f472037e59792405c5f1d7
moomin94 <aktmd2529@gmail.com>
2022-10-19 01:57:37 +0900

    Style: 서브 네비 부분

--- js/common.js
+++ js/common.js
@@ -2,6 +2,27 @@
 
 document.addEventListener("DOMContentLoaded", function(){
 
-    // Handler when the DOM is fully loaded
+    const areaBtn = document.querySelectorAll('.area > li > a');
+    const subNavArea = document.querySelectorAll('.sub-nav-area');
+    
+    console.log(areaBtn);
+
+    for(let i = 0; i < areaBtn.length; i++){
+        areaBtn[i].addEventListener('mouseover',function(){
+            subNavArea[i].classList.remove('show');
+            this.nextElementSibling.classList.add('show');
+        });
+        areaBtn[i].addEventListener('mouseout',function(){
+            this.nextElementSibling.classList.remove('show');
+        });
+        subNavArea[i].addEventListener('mouseover',function(){
+            subNavArea[i].classList.remove('show');
+            this.classList.add('show');
+        });
+        subNavArea[i].addEventListener('mouseout',function(){
+            this.classList.remove('show');
+        });
+    }
+    
 
 });
